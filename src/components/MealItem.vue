e1c1d71c462e6c839cbfa83f2885508ff57999ad
omadbek <abdullayevomadbek709@gmail.com>
2024-07-08 12:50:32 +0500

    MealItem card qoshildi qayata qayta ishlatish oson bolishi uchun

---
+++ src/components/MealItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-white shadow rounded-2xl">
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl w-full h-80 object-center object-cover" loading="lazy" />
+    <h3 class="p-4 font-bold text-[20px]">{{ meal.strMeal }}</h3>
+    <div class="p-4 flex items-center justify-between">
+      <YouTubeBtn :href="meal.strYoutube">YouTube</YouTubeBtn>
+      <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }" class="text-red-500 font-medium text-[18px]"> View </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import YouTubeBtn from "../components/YouTubeBtn.vue";
+
+const { meal } = defineProps({
+  meal: Object,
+});
+</script>
--- src/views/MealByName.vue
+++ src/views/MealByName.vue
@@ -4,14 +4,7 @@
   </div>
 
   <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <div v-for="meal in meals" :key="meal.idMeal" class="bg-white shadow rounded-2xl">
-      <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl w-full h-80 object-center object-cover" loading="lazy" />
-      <h3 class="p-4 font-bold text-[20px]">{{ meal.strMeal }}</h3>
-      <div class="p-4 flex items-center justify-between">
-        <YouTubeBtn :href="meal.strYoutube" >YouTube</YouTubeBtn>
-        <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }" class="text-red-500 font-medium text-[18px]"> View </router-link>
-      </div>
-    </div>
+  <MealItem v-for="meal in meals" :key="meal.idMeal" :meal="meal"/>
   </div>
 </template>
 
@@ -19,8 +12,7 @@
 import { computed, onMounted, ref } from "vue";
 import store from "../store";
 import { useRoute } from "vue-router";
-import YouTubeBtn from "../components/YouTubeBtn.vue";
-
+import MealItem from "../components/MealItem.vue";
 const router = useRoute();
 const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
